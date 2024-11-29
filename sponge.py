99a3b8c20cfcfc39d5005bf225aa3cb00d2e15ca
Elzara Turakulova <endjik@gmail.com>
2024-11-29 15:16:40 -0800

    initial solution

--- sponge.py
+++ sponge.py
@@ -1,11 +1,21 @@
 def sponge_case(sentence):
     # Write your solution here!
-    pass
+    word_arr = sentence.split()
+    new_arr = []
 
+    for word in word_arr:
+        scramble = ""
+        for i, letter in enumerate(word):
+            if i % 2 == 0:
+                scramble += letter.lower()
+            else:
+                scramble += letter.upper()
+        new_arr.append(scramble)
+    return (' ').join(new_arr)
 
 
 # Test cases
-assert sponge_case("spongebob") == "sPoNgEbOb"
+# assert sponge_case("spongebob") == "sPoNgEbOb"
 assert sponge_case("Who are YOU calling A Pinhead") == "wHo aRe yOu cAlLiNg a pInHeAd"
 assert sponge_case("WHAT is UP my dude") == "wHaT iS uP mY dUdE"
 assert sponge_case("E") == "e"
